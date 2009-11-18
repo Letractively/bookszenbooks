d4cae3586c562e69e0fbcf5b9aed119f591f2b3e
ricjustsaid@gmail.com <ricjustsaid@gmail.com@fc342400-cc30-11de-8626-0d8cb8b546e2>
2009-11-18 14:44:50 +0000

    - Refactored entire project into fewer packages. - Created /assets/ directory for web resource files. - SQL statements are included in /WEB-INF - must be run if you want to play around with the database. - Fixed issue with selecting the incorrect database with SQL queries - added DBDriverMySQL.getFullTableName(String) method. - Fixed potential issue with UPDATE/DELETE statements and LIMIT clause. LIMITs are not applied if a 0/NULL value is supplied for the limit parameter in update() and delete() queries. - Fixed capitalization issue in DBObject.populate(). Mutators work now! - Split DBObject.populate() into multiple methods to keep the method clean. - DBObject.populate() now handled double datatype. - All subclasses of DBObject now implement Serializable interface since they will be used as JavaBeans. - Major cleanup of class variables, removed unneeded references to related objects, added "dirty" and "newObject" variables. - Added 2 new fields to DBObject: primaryKeys and tableName.

--- trunk/src/org/bzb/db/entity/book/BookCategory.java
+++
@@ -1,9 +0,0 @@
-package org.bzb.db.entity.book;
-
-import org.bzb.db.entity.DBObject;
-
-public class BookCategory extends DBObject {
-	private int categoryId;
-	private String name;
-	Book unnamed_Book_;
-}
--- trunk/src/org/bzb/db/entity/i18n/LexiconTopic.java
+++
@@ -1,9 +0,0 @@
-package org.bzb.db.entity.i18n;
-
-import java.util.ArrayList;
-import org.bzb.db.entity.DBObject;
-
-public class LexiconTopic extends DBObject {
-	private String name;
-	ArrayList<LexiconEntry> lexiconEntry = new ArrayList<LexiconEntry>();
-}
