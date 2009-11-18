8cf4fcb40702cbfe2085c7ebb9ec67d668f87df7
ricjustsaid <ricjustsaid@gmail.com>
2009-11-18 14:44:50 +0000

    - Refactored entire project into fewer packages. - Created /assets/ directory for web resource files. - SQL statements are included in /WEB-INF - must be run if you want to play around with the database. - Fixed issue with selecting the incorrect database with SQL queries - added DBDriverMySQL.getFullTableName(String) method. - Fixed potential issue with UPDATE/DELETE statements and LIMIT clause. LIMITs are not applied if a 0/NULL value is supplied for the limit parameter in update() and delete() queries. - Fixed capitalization issue in DBObject.populate(). Mutators work now! - Split DBObject.populate() into multiple methods to keep the method clean. - DBObject.populate() now handled double datatype. - All subclasses of DBObject now implement Serializable interface since they will be used as JavaBeans. - Major cleanup of class variables, removed unneeded references to related objects, added "dirty" and "newObject" variables. - Added 2 new fields to DBObject: primaryKeys and tableName.

--- src/org/bzb/model/web/CheckoutServlet.java
+++
@@ -1,9 +0,0 @@
-package org.bzb.model.web;
-
-import javax.servlet.http.HttpServletRequest;
-import org.bzb.db.DBDriver;
-
-public class CheckoutServlet {
-	private HttpServletRequest request;
-	private DBDriver db;
-}
